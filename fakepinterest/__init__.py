8c6e8dc34ab5e677b124eaf3ac3d8c5a3e5f7adb
Rodrigo Reis <suporteproc@alema.local>
2024-03-25 10:52:23 -0400

    first commit

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SECRET_KEY'] = 'bee04e348ba4a5cf094914aede7a6f61'
+app.config['UPLOAD_FOLDER'] = 'static/fotos_posts'
+
+
+
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'homepage'
+
+from fakepinterest import routes
---
+++ fakepinterest/routes.py
@@ -0,0 +1,84 @@
+
+from flask import render_template, url_for, redirect
+from fakepinterest import app,database,bcrypt
+from flask_login import login_required, login_user, logout_user, current_user
+from fakepinterest.forms import FormCriarConta, FormLogin, FormFoto
+from fakepinterest.models import Usuario, Foto
+import os
+from werkzeug.utils import secure_filename
+
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    form_login = FormLogin()
+
+    if form_login.validate_on_submit():
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=True)
+            return redirect(url_for('perfil', id_usuario=usuario.id))
+
+            
+    return render_template('homepage.html', form=form_login)
+
+
+
+
+@app.route('/criarconta/', methods=['GET', 'POST'])
+def criar_conta():
+    form_criar_conta = FormCriarConta()
+
+    if form_criar_conta.is_submitted():
+        senha = bcrypt.generate_password_hash(form_criar_conta.senha.data)
+        usuario = Usuario(username=form_criar_conta.username.data, 
+                          email=form_criar_conta.email.data, senha=senha)
+        database.session.add(usuario)
+        database.session.commit()
+        login_user(usuario, remember=True)
+        return redirect(url_for('perfil', id_usuario=usuario.id))
+
+    return render_template('criar_conta.html', form=form_criar_conta)
+
+
+
+@app.route('/perfil/<id_usuario>', methods=['GET', 'POST'])
+@login_required
+def perfil(id_usuario):
+    usuario = Usuario.query.get(int(id_usuario))
+    if int(id_usuario) == int(current_user.id):
+        form_foto = FormFoto()
+
+        if form_foto.validate_on_submit():
+            arquivo = form_foto.foto.data
+            nome_seguro =secure_filename(arquivo.filename)
+            
+            #salvando na pasta Static
+            caminho = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                              app.config['UPLOAD_FOLDER'], nome_seguro)
+            arquivo.save(caminho)
+
+            #salvando no banco de dados
+            foto = Foto(imagem=nome_seguro, id_usuario=current_user.id)
+            database.session.add(foto)
+            database.session.commit()
+
+        return render_template('perfil_usuario.html', usuario=current_user, form=form_foto)
+    else:
+        usuario = Usuario.query.get(int(id_usuario))
+        return render_template('perfil_usuario.html', usuario=usuario, form=None)
+
+
+@app.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
+
+
+
+
+@app.route('/feed/')
+@login_required
+def feed():
+    fotos = Foto.query.order_by(Foto.criado_em.desc()).all()
+    return render_template('feed.html', fotos=fotos)
